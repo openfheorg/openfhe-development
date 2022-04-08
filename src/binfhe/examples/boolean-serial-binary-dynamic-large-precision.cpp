c59891525b83512d137d7501d2af337e3fd9ceeb
RaindropFamily <zl2967@columbia.edu>
2022-04-08 11:10:08 -0700

    addressing comments

--- src/binfhe/examples/boolean-serial-binary-dynamic-large-precision.cpp
+++ src/binfhe/examples/boolean-serial-binary-dynamic-large-precision.cpp
@@ -40,7 +40,7 @@ using namespace lbcrypto;
 // path where files will be written to
 const std::string DATAFOLDER = "demoData";
 
-int serialization_example() {
+int main() {
     // Generating the crypto context
 
     auto cc1 = BinFHEContext();
