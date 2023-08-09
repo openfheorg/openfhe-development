a9a65eee2885c6dd8a2370e5c6af1166e1428702
dsuponitskiy <dmitriy.suponitskiy@gmail.com>
2023-08-09 09:18:32 -0400

    Correction to the number of coefficients to be generated for EvalChebyshevFunction (#536)
    Co-authored-by: Dmitriy Suponitskiy <dsuponitskiy@dualitytech.com>

--- src/core/lib/math/chebyshev.cpp
+++ src/core/lib/math/chebyshev.cpp
@@ -44,17 +44,19 @@
 namespace lbcrypto {
 
 std::vector<double> EvalChebyshevCoefficients(std::function<double(double)> func, double a, double b, uint32_t degree) {
+    // the number of coefficients to be generated should be degree+1 as zero is also included
+    size_t coeffTotal{degree+1};
     double bMinusA = 0.5 * (b - a);
     double bPlusA  = 0.5 * (b + a);
-    double PiByDeg = M_PI / static_cast<double>(degree);
-    std::vector<double> functionPoints(degree);
-    for (uint32_t i = 0; i < degree; ++i)
+    double PiByDeg = M_PI / static_cast<double>(coeffTotal);
+    std::vector<double> functionPoints(coeffTotal);
+    for (size_t i = 0; i < coeffTotal; ++i)
         functionPoints[i] = func(std::cos(PiByDeg * (i + 0.5)) * bMinusA + bPlusA);
 
-    double multFactor = 2.0 / static_cast<double>(degree);
-    std::vector<double> coefficients(degree);
-    for (uint32_t i = 0; i < degree; ++i) {
-        for (uint32_t j = 0; j < degree; ++j)
+    double multFactor = 2.0 / static_cast<double>(coeffTotal);
+    std::vector<double> coefficients(coeffTotal);
+    for (size_t i = 0; i < coeffTotal; ++i) {
+        for (size_t j = 0; j < coeffTotal; ++j)
             coefficients[i] += functionPoints[j] * std::cos(PiByDeg * i * (j + 0.5));
         coefficients[i] *= multFactor;
     }
