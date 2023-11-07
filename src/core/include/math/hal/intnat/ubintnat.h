d904a264f3250858efa9a69f51244c215d992cee
Marcone Almeida <marcone.almeida@gmail.com>
2023-11-07 02:21:40 +0000

    Added support to IBM/S390(x) to intnat::MultD
    (cherry picked from commit 7a7df9eb215d7dd80df09a069b715b9ac0e96c13)

--- src/core/include/math/hal/intnat/ubintnat.h
+++ src/core/include/math/hal/intnat/ubintnat.h
@@ -1940,11 +1940,16 @@ private:
         wres   = wa * wb;  // should give us 128 bits  of 64 * 64
         res.hi = (uint64_t)(wres >> 64);
         res.lo = (uint64_t)wres;
-#elif defined(__arm__) || defined(__powerpc__) // 32 bit processor
+#elif defined(__arm__) || defined(__powerpc__)  // 32 bit processor
         uint64_t wres(0), wa(a), wb(b);
         wres   = wa * wb;
         res.hi = wres >> 32;
         res.lo = (uint32_t)wres & 0xFFFFFFFF;
+#elif __s390__
+        U128BITS wres(0), wa(a), wb(b);
+        wres   = wa * wb;
+        res.hi = (uint64_t)(wres >> 64);
+        res.lo = (uint64_t)wres;
 #elif defined(__EMSCRIPTEN__)  // web assembly
         U64BITS a1 = a >> 32;
         U64BITS a2 = (uint32_t)a;
