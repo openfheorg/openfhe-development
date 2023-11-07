cc58a13ec9093d1995b6d749e6b441f18776f8b5
Marcone Almeida <marcone.almeida@gmail.com>
2023-11-07 03:00:04 +0000

    Fix the big-endian issue with memcpy in S390/x
    (cherry picked from commit 756888703f7f879f29c92620ad15c825d8b154ef)

--- src/pke/lib/scheme/bfvrns/bfvrns-cryptoparameters.cpp
+++ src/pke/lib/scheme/bfvrns/bfvrns-cryptoparameters.cpp
@@ -67,8 +67,13 @@ void CryptoParametersBFVRNS::PrecomputeCRTTables(KeySwitchTechnique ksTech, Scal
     for (uint32_t i = 0; i < sizeQ; i++) {
         BigInteger mu = BarrettBase128Bit / BigInteger(moduliQ[i]);
         uint64_t val[2];
+#ifdef __s390__
+        val[1] = (mu % TwoPower64).ConvertToInt();
+        val[0] = mu.RShift(64).ConvertToInt();
+#else
         val[0] = (mu % TwoPower64).ConvertToInt();
         val[1] = mu.RShift(64).ConvertToInt();
+#endif
         memcpy(&m_modqBarrettMu[i], val, sizeof(DoubleNativeInt));
     }
 
@@ -138,8 +143,13 @@ void CryptoParametersBFVRNS::PrecomputeCRTTables(KeySwitchTechnique ksTech, Scal
         for (uint32_t i = 0; i < sizeR; i++) {
             BigInteger mu = BarrettBase128Bit / BigInteger(moduliR[i]);
             uint64_t val[2];
+#ifdef __s390__
+            val[1] = (mu % TwoPower64).ConvertToInt();
+            val[0] = mu.RShift(64).ConvertToInt();
+#else
             val[0] = (mu % TwoPower64).ConvertToInt();
             val[1] = mu.RShift(64).ConvertToInt();
+#endif
 
             memcpy(&m_modrBarrettMu[i], val, sizeof(DoubleNativeInt));
         }
@@ -256,8 +266,13 @@ void CryptoParametersBFVRNS::PrecomputeCRTTables(KeySwitchTechnique ksTech, Scal
         for (uint32_t j = 0; j < moduliR.size(); j++) {
             BigInteger mu = BarrettBase128Bit / BigInteger(moduliR[j]);
             uint64_t val[2];
+#ifdef __s390__
+            val[1] = (mu % TwoPower64).ConvertToInt();
+            val[0] = mu.RShift(64).ConvertToInt();
+#else
             val[0] = (mu % TwoPower64).ConvertToInt();
             val[1] = mu.RShift(64).ConvertToInt();
+#endif
 
             memcpy(&m_modrBarrettMu[j], val, sizeof(DoubleNativeInt));
         }
@@ -705,8 +720,13 @@ void CryptoParametersBFVRNS::PrecomputeCRTTables(KeySwitchTechnique ksTech, Scal
         for (uint32_t i = 0; i < m_modbskBarrettMu.size(); i++) {
             BigInteger mu = BarrettBase128Bit / BigInteger(m_moduliBsk[i]);
             uint64_t val[2];
+#ifdef __s390__
+            val[1] = (mu % TwoPower64).ConvertToInt();
+            val[0] = mu.RShift(64).ConvertToInt();
+#else
             val[0] = (mu % TwoPower64).ConvertToInt();
             val[1] = mu.RShift(64).ConvertToInt();
+#endif
             memcpy(&m_modbskBarrettMu[i], val, sizeof(DoubleNativeInt));
         }
 
