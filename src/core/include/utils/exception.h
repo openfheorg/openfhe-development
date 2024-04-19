f0c0c3b1f47b88d608e9c612461012db125655fb
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2024-04-19 18:28:03 +0200

    Finish migration to new exception type

--- src/core/include/utils/exception.h
+++ src/core/include/utils/exception.h
@@ -100,66 +100,6 @@ public:
 //   });
 // }
 // e.Rethrow();
-class openfhe_error : public std::runtime_error {
-    std::string filename;
-    int linenum;
-    std::string message;
-
-public:
-    openfhe_error(const std::string& file, int line, const std::string& what)
-        : std::runtime_error(what), filename(file), linenum(line) {
-        message = filename + ":" + std::to_string(linenum) + " " + what;
-    }
-
-    const char* what() const throw() {
-        return message.c_str();
-    }
-
-    const std::string& GetFilename() const {
-        return filename;
-    }
-    int GetLinenum() const {
-        return linenum;
-    }
-};
-
-class config_error : public openfhe_error {
-public:
-    config_error(const std::string& file, int line, const std::string& what) : openfhe_error(file, line, what) {}
-};
-
-class math_error : public openfhe_error {
-public:
-    math_error(const std::string& file, int line, const std::string& what) : openfhe_error(file, line, what) {}
-};
-
-class not_implemented_error : public openfhe_error {
-public:
-    not_implemented_error(const std::string& file, int line, const std::string& what)
-        : openfhe_error(file, line, what) {}
-};
-
-class not_available_error : public openfhe_error {
-public:
-    not_available_error(const std::string& file, int line, const std::string& what) : openfhe_error(file, line, what) {}
-};
-
-class type_error : public openfhe_error {
-public:
-    type_error(const std::string& file, int line, const std::string& what) : openfhe_error(file, line, what) {}
-};
-
-// use this error when serializing openfhe objects
-class serialize_error : public openfhe_error {
-public:
-    serialize_error(const std::string& file, int line, const std::string& what) : openfhe_error(file, line, what) {}
-};
-
-// use this error when deserializing openfhe objects
-class deserialize_error : public openfhe_error {
-public:
-    deserialize_error(const std::string& file, int line, const std::string& what) : openfhe_error(file, line, what) {}
-};
 
 class OpenFHEException : public std::exception {
     std::string m_errorDescription;
@@ -195,17 +135,7 @@ public:
     }
 };
 
-// ATTN:
-// 1. OPENFHE_THROW is to be overloaded for the period of transition to OpenFHEException only.
-// 2. After that openfhe_error, all classes derived from it and OPENFHE_THROW_OLD must be removed
-// 3. All the macros below should be removed except OPENFHE_THROW_NEW. OPENFHE_THROW_NEW should
-//    be renamed to OPENFHE_THROW
-// #define OPENFHE_THROW(expr) throw lbcrypto::OpenFHEException(expr)
-#define OPENFHE_THROW_OLD(exc, expr) throw exc(__FILE__, __LINE__, (expr))
-#define OPENFHE_THROW_NEW(expr)      throw lbcrypto::OpenFHEException(expr)
-
-#define GET_CORRECT_MACRO(_1, _2, NAME, ...) NAME
-#define OPENFHE_THROW(...)                   GET_CORRECT_MACRO(__VA_ARGS__, OPENFHE_THROW_OLD, OPENFHE_THROW_NEW)(__VA_ARGS__)
+#define OPENFHE_THROW(expr) throw lbcrypto::OpenFHEException(expr)
 
 }  // namespace lbcrypto
 
