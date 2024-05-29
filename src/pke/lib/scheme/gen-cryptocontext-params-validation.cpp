008db9c66711e42976ccff4df01ad1c2312d8602
Dmitriy Suponitskiy <dsuponitskiy@dualitytech.com>
2024-05-29 08:55:23 -0400

    Use (MAX_MODULUS_SIZE-1) as the limit for scalingModSize. It should work for any NATIVEINT

--- src/pke/lib/scheme/gen-cryptocontext-params-validation.cpp
+++ src/pke/lib/scheme/gen-cryptocontext-params-validation.cpp
@@ -45,11 +45,9 @@ void validateParametersForCryptocontext(const Params& parameters) {
         if (NOISE_FLOODING_MULTIPARTY == parameters.GetMultipartyMode()) {
             OPENFHE_THROW("NOISE_FLOODING_MULTIPARTY is not supported in CKKSRNS");
         }
-#if NATIVEINT == 64
-        if (59 < parameters.GetScalingModSize()) {
-            OPENFHE_THROW("scalingModSize > 59 is not supported in CKKSRNS for NATIVEINT=64");
+        if (MAX_MODULUS_SIZE <= parameters.GetScalingModSize()) {
+            OPENFHE_THROW("scalingModSize should be less than " + std::to_string(MAX_MODULUS_SIZE));
         }
-#endif
     }
     else if (isBFVRNS(scheme)) {
         if (0 == parameters.GetPlaintextModulus()) {
