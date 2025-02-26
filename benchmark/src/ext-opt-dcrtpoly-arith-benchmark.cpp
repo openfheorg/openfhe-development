66aeaf3e83c209980e74e54ebaba5f28f073a334
Caesar Etos <caesar.etos@gmail.com>
2025-02-26 15:57:56 -0500

    adding dcrtpoly benchmarks

--- benchmark/src/ext-opt-dcrtpoly-arith-benchmark.cpp
+++ benchmark/src/ext-opt-dcrtpoly-arith-benchmark.cpp
@@ -12,16 +12,16 @@ using namespace lbcrypto;
 // all operations are done in the NTT domain (EVAL Format), meaning, both operand polys should be in the NTT format
 // the result is also in the NTT format
 
-// The implementation of these arithmetic operations can be found here: 
-// Poly-Poly Add : https://github.com/openfheorg/openfhe-development/blob/v1.2.3/src/core/include/math/hal/intnat/mubintvecnat.h#L414
-// Poly-Poly Mul : https://github.com/openfheorg/openfhe-development/blob/v1.2.3/src/core/include/math/hal/intnat/mubintvecnat.h#L489
-// Poly-Const Mul: https://github.com/openfheorg/openfhe-development/blob/v1.2.3/src/core/lib/math/hal/intnat/mubintvecnat.cpp#L296
+// The implementation of these arithmetic operations can be found here:
+// DCRTPoly-DCRTPoly Add : https://github.com/openfheorg/openfhe-development/blob/v1.2.3/src/core/include/lattice/hal/default/dcrtpoly.h#L153
+// DCRTPoly-DCRTPoly Mul : https://github.com/openfheorg/openfhe-development/blob/v1.2.3/src/core/include/lattice/hal/default/dcrtpoly.h#L174
+// DCRTPoly-Const Mul: https://github.com/openfheorg/openfhe-development/blob/v1.2.3/src/core/include/lattice/hal/default/dcrtpoly-impl.h#L573
 
 // size in bits of the RNS moduli, 60 bits here
 constexpr uint32_t DCRTBITS = 60;
 
 int main() {
-    std::cout << "Native Poly Arithmetic Benchmark started ...\n";
+    std::cout << "DCRT Poly Arithmetic Benchmark started ...\n";
 
     // define a discrete uniform random number generator to populate the polynomial
     DiscreteUniformGeneratorImpl<NativeVector> dug;
@@ -29,24 +29,25 @@ int main() {
     // Define the ring dimension N = 2^{logN}
     // Test for logN = 10, 11, ... , 16 (and if supported, 17 as well)
     uint32_t logN = 5;
+    uint32_t num_limbs = 3;
 
     // define native integer parameters
-    // note that ILNativeParams takes as input the order m = 2*n
-    auto params = std::make_shared<ILNativeParams>(1 << (logN + 1), DCRTBITS);
+    // note that ILDCRTParams takes as input the order m = 2*n
+    auto params = std::make_shared<ILDCRTParams<BigInteger>>(1 << (logN + 1), num_limbs, DCRTBITS);
 
-    std::cout << "ring dimension: " << params->GetRingDimension() << "\n";
-    std::cout << "prime modulus : " << params->GetModulus() << "\n";
+    std::cout << "ring dimension    : " << params->GetRingDimension() << "\n";
+    std::cout << "prime modulus     : " << params->GetModulus() << "\n";
+    std::cout << "BigModulus modulus: " << params->GetBigModulus() << "\n";
+    std::cout << "number of limbs   : " << params->GetParams().size() << "\n";
 
     // create a random polynomial in coefficient representation
-    auto poly1 = NativePoly(dug, params, Format::COEFFICIENT);
-    poly1.SwitchFormat();
-    auto poly2 = NativePoly(dug, params, Format::COEFFICIENT);
-    poly2.SwitchFormat();
+    DCRTPoly dcrtpoly1(dug, params, Format::EVALUATION);
+    DCRTPoly dcrtpoly2(dug, params, Format::EVALUATION);
     auto constFactor = NativeInteger(dug.GenerateInteger());
 
-    std::cout << "poly1: " << poly1 << std::endl;
-    std::cout << "poly2: " << poly2 << std::endl;
-    std::cout << "const: " << constFactor << std::endl;
+    std::cout << "dcrtpoly1: " << dcrtpoly1 << std::endl;
+    std::cout << "dcrtpoly2: " << dcrtpoly2 << std::endl;
+    std::cout << "const    : " << constFactor << std::endl;
 
     const uint32_t num_iterations = 1000;  // Number of iterations for benchmarking
 
@@ -57,21 +58,21 @@ int main() {
 
     for (size_t i = 0; i < num_iterations; ++i) {
         auto start_sum = std::chrono::high_resolution_clock::now();
-        auto sum       = poly1 + poly2;
+        auto sum       = dcrtpoly1 + dcrtpoly2;
         auto end_sum   = std::chrono::high_resolution_clock::now();
         add_time += std::chrono::duration_cast<std::chrono::microseconds>(end_sum - start_sum).count();
         if (0 == i)
             std::cout << "sum: " << sum << std::endl;
 
         auto start_mul = std::chrono::high_resolution_clock::now();
-        auto mul       = poly1 * poly2;  // inverse ntt
+        auto mul       = dcrtpoly1 * dcrtpoly2;  // inverse ntt
         auto end_mul   = std::chrono::high_resolution_clock::now();
         mul_time += std::chrono::duration_cast<std::chrono::microseconds>(end_mul - start_mul).count();
         if (0 == i)
             std::cout << "mul: " << mul << std::endl;
 
         auto start_mulc = std::chrono::high_resolution_clock::now();
-        auto mulConst   = poly1 * constFactor;
+        auto mulConst   = dcrtpoly1 * constFactor;
         auto end_mulc   = std::chrono::high_resolution_clock::now();
         mulc_time += std::chrono::duration_cast<std::chrono::microseconds>(end_mulc - start_mulc).count();
         if (0 == i)
@@ -84,11 +85,11 @@ int main() {
     double avg_mulc_time = static_cast<double>(mulc_time) / num_iterations;
 
     // Output the results
-    std::cout << "Average time for adding 2 native polys     : " << avg_add_time << " microseconds" << std::endl;
-    std::cout << "Average time for multiplying 2 native polys: " << avg_mul_time << " microseconds" << std::endl;
-    std::cout << "Average time for native poly Mult Const    : " << avg_mulc_time << " microseconds" << std::endl;
+    std::cout << "Average time for adding 2 dcrtpolys     : " << avg_add_time << " microseconds" << std::endl;
+    std::cout << "Average time for multiplying 2 dcrtpolys: " << avg_mul_time << " microseconds" << std::endl;
+    std::cout << "Average time for dcrtpoly Mult Const    : " << avg_mulc_time << " microseconds" << std::endl;
 
-    std::cout << "Native Poly Arithmetic Benchmark terminated gracefully.\n";
+    std::cout << "DCRT Poly Arithmetic Benchmark terminated gracefully.\n";
 
     return EXIT_SUCCESS;
 }
