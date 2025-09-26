fa433834723a834151785d9c61b3ec993eb4270a
Dmitriy Suponitskiy <dsuponitskiy@dualitytech.com>
2025-09-26 13:42:52 -0400

    Removed 'sudo' from uninstall_openfhe.sh

--- scripts/uninstall_openfhe.sh
+++ scripts/uninstall_openfhe.sh
@@ -17,19 +17,19 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 function uninstall_unix() {
-    sudo xargs rm -vf < install_manifest.txt || echo Nothing in install_manifest.txt to be uninstalled!
+    xargs rm -vf < install_manifest.txt || echo Nothing in install_manifest.txt to be uninstalled!
 
     # Parse out the include directory's full path from install_manifest.txt's by using bash parameter expansion
     _inc=$(cat install_manifest.txt|grep "/include/openfhe" | head -n 1)
     match="${_inc%openfhe*}openfhe"
     echo "Removing: ${match}"
-    sudo rm -vr "${match}"
+    rm -vr "${match}"
 
     # Parse out the include directory's full path from install_manifest.txt's by using bash parameter expansion
     _lib=$(cat install_manifest.txt|grep "/lib/OpenFHE" | head -n 1)
     match="${_lib%OpenFHE*}"
     echo "Removing: ${match}"
-    sudo rm -vr "${match}"
+    rm -vr "${match}"
 
     unset _inc _lib
 }
