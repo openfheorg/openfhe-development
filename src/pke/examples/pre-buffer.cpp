54e72030b030ffe16c2c7ffec6812d6524bf0ab1
Burak Kucuktopal <burak.kucuktopal123@gmail.com>
2026-02-14 10:00:52 +0100

    Fix incomplete Issue #110 - change PREMode default to NOT_SET

--- src/pke/examples/pre-buffer.cpp
+++ src/pke/examples/pre-buffer.cpp
@@ -71,7 +71,8 @@ bool run_demo_pre(void) {
     CCParams<CryptoContextBFVRNS> parameters;
     parameters.SetPlaintextModulus(plaintextModulus);
     parameters.SetScalingModSize(60);
-
+    parameters.SetPREMode(INDCPA);
+    
     CryptoContext<DCRTPoly> cc = GenCryptoContext(parameters);
     std::cout << "\nParam generation time: "
               << "\t" << TOC_MS(t) << " ms" << std::endl;
