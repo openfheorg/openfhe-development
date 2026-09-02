25ebcd1fc36128518285929296bfb18cb0798966
Hugo Sanz González <hsanzgon@bsc.es>
2026-09-02 17:21:53 +0200

    Remove `duration` macro to not clash with `std::chrono::duration`
    The standard library implementation in NVIDIA's CCCL has began clashing
    with this OpenFHE macro. Specifically,
    [`cuda/std/__chrono/duration.h`](https://github.com/NVIDIA/cccl/blob/2c613a5efae865102ca460b7aad1482f115c08ee/libcudacxx/include/cuda/std/__chrono/duration.h)
    uses the type `duration`, which accidentally gets replaced by OpenFHE's
    macro when any CUDA header is imported after `openfhe.h`:
    
    ```
    In file included from /usr/local/cuda-13.3/targets/x86_64-linux/include/cccl/cuda/__event/timed_event.h:30,
                     from /usr/local/cuda-13.3/targets/x86_64-linux/include/cccl/cuda/__stream/stream_ref.h:28,
                     from /usr/local/cuda-13.3/targets/x86_64-linux/include/cccl/cuda/__stream/get_stream.h:26,
                     from /usr/local/cuda-13.3/targets/x86_64-linux/include/cccl/cuda/stream_ref:56,
                     from /home/hsanzgon/myusr/include/rmm/cuda_stream_view.hpp:10,
                     from /home/hsanzgon/myusr/include/rmm/device_buffer.hpp:8,
                     from /home/hsanzgon/myusr/include/HEonGPU-1.1/heongpu/util/memorypool.cuh:21,
                     from /home/hsanzgon/myusr/include/HEonGPU-1.1/heongpu/util/devicevector.cuh:13,
                     from /home/hsanzgon/myusr/include/HEonGPU-1.1/heongpu/host/bfv/context.cuh:11,
                     from /home/hsanzgon/myusr/include/HEonGPU-1.1/heongpu/heongpu.hpp:9,
                     from /home/hsanzgon/ckks-benchmark/include/backends/heongpu.h:4,
                     from /home/hsanzgon/ckks-benchmark/include/backends/impls.h:11,
                     from /home/hsanzgon/ckks-benchmark/include/tensor.h:9,
                     from /home/hsanzgon/ckks-benchmark/include/utils/tracing.h:10,
                     from /home/hsanzgon/ckks-benchmark/src/backends/openfhe.cpp:15:
    /usr/local/cuda-13.3/targets/x86_64-linux/include/cccl/cuda/std/__chrono/duration.h:237:68: error: macro "duration" passed 2 arguments, but takes just 1
      237 |   _CCCL_API constexpr duration(const duration<_Rep2, _Period2>& __d)
          |                                                                    ^
    In file included from /home/hsanzgon/myusr/include/openfhe/core/math/hal/intnat/ubintnat.h:44,
                     from /home/hsanzgon/myusr/include/openfhe/core/math/hal/nativeintbackend.h:36,
                     from /home/hsanzgon/myusr/include/openfhe/core/math/math-hal.h:49,
                     from /home/hsanzgon/myusr/include/openfhe/core/lattice/hal/elemparams.h:39,
                     from /home/hsanzgon/myusr/include/openfhe/core/lattice/hal/default/ilparams.h:39,
                     from /home/hsanzgon/myusr/include/openfhe/core/lattice/hal/lat-backend.h:49,
                     from /home/hsanzgon/myusr/include/openfhe/core/lattice/lat-hal.h:35,
                     from /home/hsanzgon/myusr/include/openfhe/core/openfhecore.h:39,
                     from /home/hsanzgon/myusr/include/openfhe/pke/openfhe.h:40,
                     from /home/hsanzgon/ckks-benchmark/include/backends/openfhe.h:4,
                     from /home/hsanzgon/ckks-benchmark/src/backends/openfhe.cpp:1:
    /home/hsanzgon/myusr/include/openfhe/core/utils/debug.h:198:9: note: macro "duration" defined here
      198 | #define duration(a)    std::chrono::duration_cast<std::chrono::milliseconds>(a).count()
          |         ^~~~~~~~
    ```
    
    Work performed at AccelCom, Barcelona Supercomputing Center.

--- src/core/include/utils/debug.h
+++ src/core/include/utils/debug.h
@@ -121,10 +121,10 @@
     #endif  // PROFILE
 
     #define TIC(t)    t = timeNow()
-    #define TOC(t)    duration(timeNow() - t)
     #define TOC_NS(t) duration_ns(timeNow() - t)
     #define TOC_US(t) duration_us(timeNow() - t)
     #define TOC_MS(t) duration_ms(timeNow() - t)
+    #define TOC(t)    TOC_MS(t)
 
 #else  // NDEBUG
 
@@ -184,10 +184,10 @@
             } while (0)
 
         #define TIC(t)    t = timeNow()
-        #define TOC(t)    duration(timeNow() - t)
         #define TOC_NS(t) duration_ns(timeNow() - t)
         #define TOC_US(t) duration_us(timeNow() - t)
         #define TOC_MS(t) duration_ms(timeNow() - t)
+        #define TOC(t)    TOC_MS(t)
 
     #endif  // PROFILE
 
@@ -195,7 +195,6 @@
 
 typedef std::chrono::high_resolution_clock::time_point TimeVar;
 
-#define duration(a)    std::chrono::duration_cast<std::chrono::milliseconds>(a).count()
 #define duration_ns(a) std::chrono::duration_cast<std::chrono::nanoseconds>(a).count()
 #define duration_us(a) std::chrono::duration_cast<std::chrono::microseconds>(a).count()
 #define duration_ms(a) std::chrono::duration_cast<std::chrono::milliseconds>(a).count()
@@ -207,7 +206,7 @@ template <typename F, typename... Args>
 double funcTime(F func, Args&&... args) {
     TimeVar t1 = timeNow();
     func(std::forward<Args>(args)...);
-    return duration(timeNow() - t1);
+    return duration_ms(timeNow() - t1);
 }
 
 #endif  // #__dbg_h__
